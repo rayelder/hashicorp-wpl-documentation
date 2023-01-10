2e3533e752356af5d779a5c5cc4b3c48176ef5d0
Ray Elder <ray@hashicorp.com>
2023-01-10 09:52:42 -0800

    Update all packages

---
+++ theme.config.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default {
+  project: {
+    link: 'https://hashicorp-wpl-documentation.vercel.app/',
+  },
+  logo: <strong>HashiCorp WPL</strong>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – HashiCorp WPL',
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="HashiCorp WPL" />
+      <meta
+        property="og:description"
+        content="Documentation for HashiCorp WPL"
+      />
+    </>
+  ),
+  primaryHue: { dark: 220, light: 220 },
+  sidebar: { defaultMenuCollapseLevel: 0 },
+  footer: {
+    text: (
+      <span>
+        ©{new Date().getFullYear()}{' '}
+        <a
+          href="https://hashicorp-wpl-documentation.vercel.app/"
+          target="_blank"
+        >
+          HashiCorp, Inc.
+        </a>
+        &nbsp; All rights reserved.
+      </span>
+    ),
+  },
+}
